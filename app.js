5b0329c1fa6c49f9adc77e2a5874778284a5c4fb
Harsh <patelharsh749005@gmail.com>
2025-05-20 12:03:18 +0530

    flash messages are added and index route is created

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ const ownersRouter = require("./routes/ownersRouter")
 const usersRouter = require("./routes/usersRouter")
 const productsRouter = require("./routes/productsRouter")
 const db = require('./config/mongoose-connection');
-
+const indexRouter = require("./routes/index");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
 require("dotenv").config();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use(
+  expressSession({
+    resave:false,
+    saveUninitialized:false,
+    secret:process.env.EXPRESS_SESSION_SECRECT,
+  })
+);
+app.use(flash());
 app.use(express.static(path.join(__dirname,"public")))
 app.set("view engine","ejs");
 
