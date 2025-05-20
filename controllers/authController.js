78b8fac5e9413301cca73b0dbf8614cc6e4a376c
Harsh <patelharsh749005@gmail.com>
2025-05-20 10:22:19 +0530

    user account already exist , condition check

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,10 +3,12 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken")
 const {generateToken} = require("../utils/generateToken")
 
-module.exports.registerUser =  (req, res) => {
+module.exports.registerUser =  async (req, res) => {
   // http://localhost:3000/users/register for postman
   try {
     let { email, password, fullname } = req.body;
+    let user = await userModel.findOne({email:email});
+    if(user) return res.status(401).send("You already have an account, please login");
     bcrypt.genSalt(10, function (err, salt) {
       bcrypt.hash(password, salt, async (err, hash) => {
         if (err) return res.send(err.message);
