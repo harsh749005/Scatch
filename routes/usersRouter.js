df3f4a7d6fa14a76af8615438c6c64d8f8d2de88
Harsh Patel <patelharsh749005@gmail.com>
2025-05-20 11:10:02 +0530

    login route is created (#1)

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,12 +1,14 @@
 // kabab case,camel case and pascel case
 const express = require("express");
 const router = express.Router();
-const {registerUser} = require("../controllers/authController")
+const {registerUser,loginUser} = require("../controllers/authController")
 router.get("/", (req, res) => {
   res.send("hey it's working"); // response handler and error handler
 });
 
 router.post("/register",registerUser);
+
+router.post("/login",loginUser);
 
 module.exports = router;
 
