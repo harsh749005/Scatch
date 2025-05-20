9585564edb7ee98dd22d62d979ca5284336a9080
Harsh <patelharsh749005@gmail.com>
2025-05-20 11:57:18 +0530

    middleware is created

---
+++ middleware/isLoggedIn.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user-model");
+
+module.exports.isLoggedIn = async function (req, res, next) {
+  if (!req.cookie.token) {
+    res.flash("error", "you need to login first");
+    return res.redirect("/");
+  }
+  try {
+    let decoded = jwt.verify(req.cookie.token, process.env.JWT_KEY);
+    let user = await userModel
+      .findOne({ email: decoded.email })
+      .select("--password");
+    req.user = user;
+    next();
+  } catch (error) {
+    req.flash("error", "something went wrong");
+    res.redirect("/");
+  }
+};
